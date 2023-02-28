cb550c95161b1140493d08681035d0c2de33ad3b
IvanKord <117990934+IvanKord@users.noreply.github.com>
2023-02-28 09:03:06 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,11 +1,11 @@
 a = int(input("Введите 4х значное число:"))
 if a < 1000 or a > 9999:
-print("плохое число")
+    print("плохое число")
 else:
-a4 = a % 10
-a3 = a // 10 % 10
-a2 = a // 100 % 10
-a1 = a // 100 // 10
-Sum = a1 + a2 + a3 + a4
-Pr = a1 * a2 * a3 * a4
-print(Sum, Pr)
+    a4 = a % 10
+    a3 = a // 10 % 10
+    a2 = a // 100 % 10
+    a1 = a // 100 // 10
+    Sum = a1 + a2 + a3 + a4
+    Pr = a1 * a2 * a3 * a4
+    print(Sum, Pr)
