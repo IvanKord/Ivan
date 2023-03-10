f2a3b372d9717e1c0b700f90011a72f7df70c5c1
IvanKord <117990934+IvanKord@users.noreply.github.com>
2023-03-10 11:27:07 +0300

    пр.6 от 06.03.2023

--- 24.py
+++ 24.py
@@ -1,5 +1,7 @@
-a, b = float(input()), float(input())
-if a < b:
-    print(a)
-else:
+a, b = float(input('впишите значение:')), float(input('впишите значение:'))
+if a > b:
     print('меньшее значение', b)
+elif a == b:
+    print('значения равны')
+else:
+    print('меньшее значение', a)
